0007338bfce996cd599c98b159c787b028954a2b
v3bc_ <onepngszymuss@gmail.com>
2026-08-22 21:57:43 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.github.xAv3nGer"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
---
+++ src/main/java/pl/v3bc/platform/Main.java
@@ -0,0 +1,9 @@
+package pl.v3bc.platform;
+
+/**
+ * @Author: v3bc_
+ * @Date: 8/22/26
+ * @Project: astra-platform
+ */
+public class Main {
+}
