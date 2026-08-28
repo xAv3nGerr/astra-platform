7008f5afb544429d01bd72273b59c030fc161bca
xAv3nGer <onepngszymuss@gmail.com>
2026-08-29 00:28:39 +0200

    fixx

--- src/main/java/pl/v3bc/platform/registry/configs/serdes/ItemsSerdesPack.java
+++ src/main/java/pl/v3bc/platform/registry/configs/serdes/ItemsSerdesPack.java
@@ -2,10 +2,13 @@ package pl.v3bc.platform.registry.configs.serdes;
 
 import eu.okaeri.configs.serdes.OkaeriSerdesPack;
 import eu.okaeri.configs.serdes.SerdesRegistry;
+import eu.okaeri.configs.yaml.bukkit.serdes.itemstack.ItemStackFailsafe;
+import eu.okaeri.configs.yaml.bukkit.serdes.serializer.ItemStackSerializer;
 
 public class ItemsSerdesPack implements OkaeriSerdesPack {
     @Override
     public void register(SerdesRegistry registry) {
+        registry.register(new ItemStackSerializer(ItemStackFailsafe.NONE));
         registry.register(new MiniMessageItemMetaSerializer());
     }
 }
