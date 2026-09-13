d5718d35d25c7f3adcb2c12ad0fac3c03f6aef3d
xAv3nGer <onepngszymuss@gmail.com>
2026-09-13 16:25:31 +0200

    fix ItemNbt

--- src/main/java/pl/v3bc/platform/utils/nbt/ItemNbt.java
+++ src/main/java/pl/v3bc/platform/utils/nbt/ItemNbt.java
@@ -5,15 +5,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import pl.v3bc.platform.Main;
-
-/**
- * @Author: v3bc_
- * @Date: 8/26/26
- * @Project: astra-platform
- */
 
 public final class ItemNbt {
+
+    private static final String NAMESPACE = "astra";
 
     public static boolean hasCustomData(ItemStack itemStack, String string, PersistentDataType persistentDataType) {
         ItemMeta itemMeta = itemStack.getItemMeta();
@@ -21,7 +16,7 @@ public final class ItemNbt {
             return false;
         }
         PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
-        return persistentDataContainer.has(new NamespacedKey(Main.getInstance(), string), persistentDataType);
+        return persistentDataContainer.has(new NamespacedKey(NAMESPACE, string), persistentDataType);
     }
 
     public static Object getCustomData(ItemStack itemStack, String string, PersistentDataType persistentDataType) {
@@ -30,14 +25,14 @@ public final class ItemNbt {
             return null;
         }
         PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
-        return persistentDataContainer.get(new NamespacedKey(Main.getInstance(), string), persistentDataType);
+        return persistentDataContainer.get(new NamespacedKey(NAMESPACE, string), persistentDataType);
     }
 
     public static ItemStack withCustomData(ItemStack itemStack, String string, Object object, PersistentDataType persistentDataType) {
         ItemMeta itemMeta = itemStack.getItemMeta();
         if (itemMeta != null) {
             PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
-            persistentDataContainer.set(new NamespacedKey(Main.getInstance(), string), persistentDataType, object);
+            persistentDataContainer.set(new NamespacedKey(NAMESPACE, string), persistentDataType, object);
             itemStack.setItemMeta(itemMeta);
         }
         return itemStack;
